4f9f0aeac0e072340243a23a3cc08a4349630841
HelAsia <asiaheluszka1993@gmail.com>
2018-02-02 22:59:21 +0100

    First commit

---
+++ main/java/com/introtoandroid/parisview/AnimationActivity.java
@@ -0,0 +1,34 @@
+package com.introtoandroid.parisview;
+
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+public class AnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation);
+
+        final Button now = (Button) findViewById(R.id.do_flagi);
+        final ImageView iv = (ImageView)findViewById(R.id.flag_anim2);
+        now.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                iv.setBackgroundResource(R.drawable.juggle);
+
+                AnimationDrawable frameAnimation = (AnimationDrawable) iv.getBackground();
+                frameAnimation.start();
+
+            }
+        });
+
+    }
+}
---
+++ main/java/com/introtoandroid/parisview/MainActivity.java
@@ -0,0 +1,44 @@
+package com.introtoandroid.parisview;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Context contex;
+    Button przycisk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView flagImageView = (ImageView)findViewById(R.id.ImageView01);
+        flagImageView.setImageResource(R.drawable.flag);
+        Animation an = AnimationUtils.loadAnimation(this,R.anim.spin);
+        flagImageView.startAnimation(an);
+
+        przycisk = (Button) findViewById(R.id.button);
+       przycisk.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                contex = getApplicationContext();
+
+                Intent intent = new Intent(contex, AnimationActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
