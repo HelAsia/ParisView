2241d39794388a9a0e8f29926c7256de2d126ee0
HelAsia <asiaheluszka1993@gmail.com>
2018-02-16 21:19:25 +0100

    Added third fragment and used all of fragments in application

--- main/java/com/introtoandroid/parisview/FragmentActivity.java
+++ main/java/com/introtoandroid/parisview/FragmentActivity.java
@@ -6,28 +6,46 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class FragmentActivity extends Activity {
+
+    Button firstFragment, secondFragment, thirdFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+        firstFragment = (Button) findViewById(R.id.button1);
+        secondFragment = (Button) findViewById(R.id.button2);
+        thirdFragment = (Button) findViewById(R.id.button3);
+
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentOne());
+            }
+        });
+
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentTwo());
+            }
+        });
+
+        thirdFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentThree());
+            }
+        });
     }
 
-    public void selectFrag(View view) {
-        Fragment fr;
-
-        if(view == findViewById(R.id.button2)) {
-            fr = new FragmentTwo();
-
-        }else {
-            fr = new FragmentOne();
-        }
-
+    private void loadFragment(Fragment fragment) {
         FragmentManager fm = getFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_place, fr);
+        fragmentTransaction.replace(R.id.fragment_place, fragment);
         fragmentTransaction.commit();
 
     }
